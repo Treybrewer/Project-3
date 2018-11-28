a639a2d3a5c1a3027481e6eba45f164429928124
holland cookson <hollandcookson@hollands-MacBook-Pro-2.local>
2018-11-28 18:53:59 -0500

    input.js etc

--- project3/client/src/pages/Input/Input.js
+++ project3/client/src/pages/Input/Input.js
@@ -1,118 +1,69 @@
-import React, { Component } from "react";
-import DeleteBtn from "../../components/DeleteBtn";
-import Jumbotron from "../../components/Jumbotron";
-import API from "../../utils/API";
-import { Link } from "react-router-dom";
-import { Col, Row, Container } from "../../components/Grid";
-import { List, ListItem } from "../../components/List";
-import { Input, TextArea, FormBtn } from "../../components/Form";
+import React, {Component} from 'react';  
+import API from '../../utils/API';
 
-class Input extends Component {
+/* Import Components */
+
+export default class Form extends React.Component {
   state = {
-    books: [],
-    title: "",
-    author: "",
-    synopsis: ""
-  };
-
-  componentDidMount() {
-    this.loadBooks();
+    company: '',
+    searchWords: '',
+    input: ''
   }
 
-  loadBooks = () => {
-    API.getBooks()
-      .then(res =>
-        this.setState({ books: res.data, title: "", author: "", synopsis: "" })
-      )
-      .catch(err => console.log(err));
-  };
-
-  deleteBook = id => {
-    API.deleteBook(id)
-      .then(res => this.loadBooks())
-      .catch(err => console.log(err));
-  };
-
-  handleInputChange = event => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-  };
-
-  handleFormSubmit = event => {
-    event.preventDefault();
-    if (this.state.title && this.state.author) {
-      API.saveBook({
-        title: this.state.title,
-        author: this.state.author,
-        synopsis: this.state.synopsis
-      })
-        .then(res => this.loadBooks())
-        .catch(err => console.log(err));
-    }
-  };
-
-  render() {
-    return (
-      <Container fluid>
-        <Row>
-          <Col size="md-6">
-            <Jumbotron>
-              <h1>What Books Should I Read?</h1>
-            </Jumbotron>
-            <form>
-              <Input
-                value={this.state.title}
-                onChange={this.handleInputChange}
-                name="title"
-                placeholder="Title (required)"
-              />
-              <Input
-                value={this.state.author}
-                onChange={this.handleInputChange}
-                name="author"
-                placeholder="Author (required)"
-              />
-              <TextArea
-                value={this.state.synopsis}
-                onChange={this.handleInputChange}
-                name="synopsis"
-                placeholder="Synopsis (Optional)"
-              />
-              <FormBtn
-                disabled={!(this.state.author && this.state.title)}
-                onClick={this.handleFormSubmit}
-              >
-                Submit Book
-              </FormBtn>
-            </form>
-          </Col>
-          <Col size="md-6 sm-12">
-            <Jumbotron>
-              <h1>Books On My List</h1>
-            </Jumbotron>
-            {this.state.books.length ? (
-              <List>
-                {this.state.books.map(book => (
-                  <ListItem key={book._id}>
-                    <Link to={"/books/" + book._id}>
-                      <strong>
-                        {book.title} by {book.author}
-                      </strong>
-                    </Link>
-                    <DeleteBtn onClick={() => this.deleteBook(book._id)} />
-                  </ListItem>
-                ))}
-              </List>
-            ) : (
-              <h3>No Results to Display</h3>
-            )}
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+change = (event) => {
+  this.setState({
+    [event.target.name]: event.target.value
+  });
 }
 
-export default Input;
+onSubmit = event => {
+  event.preventDefault();
+  API.addNewCompany({
+    company: this.state.company,
+    searchWords: this.state.searchWords
+  })
+  console.log("hello I am showing up");
+  this.setState({
+    company: '',
+    searchWords: '',
+    input: ''
+  })
+}
+
+render() {
+  return (
+  <form>                                           
+      <input
+      name='company' 
+      placeholder='company name' 
+      value={this.state.company} 
+      onChange={event => this.change(event)} 
+      />
+      <br />
+       <input
+      name='searchWords' 
+      placeholder='key words' 
+      value={this.state.searchWords} 
+      onChange={event => this.change(event)}
+      />
+      <br />
+      <input
+      name='input' 
+      placeholder='input' 
+      value={this.state.input} 
+      onChange={event => this.change(event)}
+      />
+      <br /> 
+      <input
+      name='company' 
+      placeholder='company name' 
+      value={this.state.company} 
+      onChange={event => this.change(event)}
+      />
+      <br /> 
+
+      <button onClick={this.onSubmit}>Submit</button>
+    </form>
+   );
+ }
+}
