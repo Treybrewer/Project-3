7caf06132ffba8d10a29ee4a91401f96e4999458
holland cookson <hollandcookson@hollands-MacBook-Pro-2.local>
2018-12-05 20:02:34 -0500

    a whole bunch of stuff

--- project3/client/src/components/Nav/Nav.js
+++ project3/client/src/components/Nav/Nav.js
@@ -1,17 +1,40 @@
 import React from "react";
 import "./nav.css";
-const Nav = () => (
-  <nav>
-   <div className="topNav">
-    <a href="/">HorseShoe Business Solutions</a>
-     {/* <a href="/">Home</a> */}
-     <a href="/input">Input</a>
-     <a href="/update">Update</a>
-     <a href="/results">Search</a>
-     <a href="/admin">Admin</a>
-    </div>
+// nav-bar functionality
 
-    </nav>
-);
+export default class Nav extends React.Component {
+  componentDidMount() {
+    const navBar = document.getElementById('topNav'); 
+    window.onscroll = function() {
+        if (window.pageYOffset > 100) {
+            navBar.style.background = "#789198";
+            navBar.style.boxShadow = "0px 4px 2px #33333"
+        } else  {
+            navBar.style.background = "transparent";
+            navBar.style.boxShadow = "0px 4px 2px #33333"
+        } 
+    }
+  }
+  componentDidUnmount() {
+    window.onscroll=function() {
+      undefined
+    }
+  }
+  render() {
+    return (
+      <nav>
+        <div id="topNav">
+          <a href="/"><span id="companyName">HorseShoe Business Solutions</span></a>
+          {/* <a href="/">Home</a> */}
+          <a href="/input">Input</a>
+          <a href="/update">Update</a>
+          <a href="/results">Search</a>
+          <a href="/admin">Admin</a>
+          </div>
 
-export default Nav;
+      </nav>
+    );
+  }
+}
+
+
--- project3/client/src/pages/Home/Home.js
+++ project3/client/src/pages/Home/Home.js
@@ -1,20 +1,42 @@
-import React, {Component} from 'react';  
+import React, { Component } from 'react';
 import API from '../../utils/API';
 import "./home.css";
+import image from "./iconfinder_General_Office_18_3592864.png";
+import clock from "./iconfinder_General_Office_44_3592849.png";
+import WebFont from 'webfontloader';
 
+WebFont.load({
+    google: {
+        families: ['Nunito Sans: 300,400,700', 'sans-serif']
+    }
+})
 /* Import Components */
 
 export default class Home extends React.Component {
- 
 
-render() {
-  return (
-    <div>
-      <br/>
-    <h1>Home page</h1>
-    <h3>HorseShoe Data Solutions</h3>
 
-    </div>
-   );
- }
+  render() {
+    return (
+      <div>
+        <div id="hero">
+          <div id="hero-overlay">
+            <div id="blur-section">
+            <h1> HORSESHOE SOLUTIONS</h1>
+            <h2> All of your business sampling needs </h2>
+          </div>
+            </div>
+        </div>
+        <section id="section-a">
+          <img src={image} />
+            <h2>Offering business solutions that cause greater efficiency in time and labor implementing our propietary application.</h2>
+       </section>
+       <section id="section-b">
+       <img src={clock} />
+            <h2>Implementing the Horshoe product will cut time spent on unwanted tasks and help free up your most valuable asset, time.</h2>
+       </section>
+
+
+      </div>
+    );
+  }
 }
