b20eee364c38f027111558dbcda482f4fb45e67d
holland cookson <hollandcookson@hollands-MacBook-Pro-2.local>
2018-12-11 17:13:00 -0500

    footer/nav/home/

--- project3/client/src/pages/Home/Home.js
+++ project3/client/src/pages/Home/Home.js
@@ -3,6 +3,9 @@ import API from '../../utils/API';
 import "./home.css";
 import WebFont from 'webfontloader';
 import Foot from '../../components/Foot';
+import Image from './graph.svg'
+import Image2 from './networking.svg'
+import Image3 from './evaluation.svg'
 
 WebFont.load({
   google: {
@@ -17,12 +20,22 @@ export default class Home extends React.Component {
     return (
       <div>
         <div id="hero">
-            <div id="blur-section">
-              <h1> HORSESHOE SOLUTIONS</h1>
-              <h2> All of your business sampling needs </h2>
-            </div>
+          <div id="blur-section">
+            <h1> HORSESHOE SOLUTIONS</h1>
+            <h2> All of your business sampling needs </h2>
+          </div>
         </div>
-
+        <div id="section-content">
+          <div id="image-container">
+            <img src={Image} alt="graph picture"></img> 
+            <img src={Image2} alt="team picture"></img> 
+            <img src={Image3} alt="evaluation picture"></img>
+          </div>
+          <h3>Using our business solution will save you and your company countless hours of labor. 
+            With the simple implementation of our application you will see results of greater efficiency in planning and workflow.
+            Create More Filler Text</h3>
+        </div>
+       
 
 
         <Foot></Foot>
